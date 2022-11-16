a15a23b61be48e3a06617942956337aad33d5502
Christian Murphy <christian.murphy.42@gmail.com>
2022-11-16 10:27:46 -0700

    test: initial tests with esm implementation
    leverage node:test and node:assert

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import {lintRule} from "unified-lint-rule"
-import {visit} from "unist-util-visit"
-import checkLinks  from "check-links"
-import isOnline from "is-online"
+import {lintRule} from 'unified-lint-rule'
+import {visit} from 'unist-util-visit'
+import checkLinks from 'check-links'
+import isOnline from 'is-online'
 
 /**
  * @typedef {import('mdast').Root} Root
@@ -18,66 +18,67 @@ import isOnline from "is-online"
 
 /** @type {import('unified-lint-rule').Rule<Root, Options>} */
 function noDeadUrls(ast, file, options) {
-    /** @type {{[url: string]: Array<Link | Image | Definition>}} */
-  const urlToNodes = {};
-
+  /** @type {{[url: string]: Array<Link | Image | Definition>}} */
+  const urlToNodes = {}
 
   visit(ast, ['link', 'image', 'definition'], (node) => {
-    if (!('url' in node) || !node.url) return;
-    const url = node.url;
+    if (!('url' in node) || !node.url) return
+    const url = node.url
     if (
       options.skipLocalhost &&
       /^(https?:\/\/)(localhost|127\.0\.0\.1)(:\d+)?/.test(url)
     ) {
-      return;
+      return
     }
+
     if (
       options.skipUrlPatterns &&
       options.skipUrlPatterns.some((skipPattern) =>
         new RegExp(skipPattern).test(url)
       )
     ) {
-      return;
+      return
     }
 
     if (!urlToNodes[url]) {
-      urlToNodes[url] = [];
+      urlToNodes[url] = []
     }
 
-    urlToNodes[url].push(node);
-  });
+    urlToNodes[url].push(node)
+  })
 
   return checkLinks(Object.keys(urlToNodes), options.gotOptions).then(
     (results) => {
-      Object.keys(results).forEach((url) => {
-        const result = results[url];
-        if (result.status !== 'dead') return;
+      for (const url of Object.keys(results)) {
+        const result = results[url]
+        if (result.status !== 'dead') continue
 
-        const nodes = urlToNodes[url];
-        if (!nodes) return;
+        const nodes = urlToNodes[url]
+        if (!nodes) continue
 
         for (const node of nodes) {
-          file.message(`Link to ${url} is dead`, node);
+          file.message(`Link to ${url} is dead`, node)
         }
-      });
+      }
     }
-  );
+  )
 }
 
 /** @type {import('unified-lint-rule').Rule<Root, Options>} */
-function wrapper(ast, file, options) {
-  options = options || {};
+function wrapper(ast, file, options = {}) {
   return isOnline().then((online) => {
     if (!online) {
       if (!options.skipOffline) {
-        file.message('You are not online and have not set skipOffline: true.');
+        file.message('You are not online and have not set skipOffline: true.')
       }
-      return;
+
+      return
     }
-    return noDeadUrls(ast, file, options);
-  });
+
+    return noDeadUrls(ast, file, options)
+  })
 }
 
-const remarkLintNotDeadLinks = lintRule('remark-lint:no-dead-urls', wrapper);
+const remarkLintNoDeadLinks = lintRule('remark-lint:no-dead-urls', wrapper)
 
-export default remarkLintNotDeadLinks;
+export default remarkLintNoDeadLinks
--- test.js
+++ test.js
@@ -1,234 +1,227 @@
-'use strict';
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import {remark} from 'remark'
+import remarkLintNoDeadLinks from './index.js'
 
-jest.mock('check-links', () => {
-  return jest.fn(() => Promise.resolve({}));
-});
+/**
+ * Wrapper for calling remark with the linter plugin
+ * @param {string} markdown
+ * @param {import("./index.js").Options} [options]
+ * @returns {Promise<import("vfile").VFile>}
+ */
+function processMarkdown(markdown, options = {}) {
+  return remark().use(remarkLintNoDeadLinks, options).process(markdown)
+}
 
-jest.mock('is-online', () => {
-  return jest.fn(() => Promise.resolve(true));
-});
+test('works with no URLs', async () => {
+  const vfile = await processMarkdown(`
+# Title
 
-const remark = require('remark');
-const dedent = require('dedent');
-const checkLinks = require('check-links');
-const isOnline = require('is-online');
-const plugin = require('.');
+No URLs in here.
+`)
+  assert.equal(vfile.messages.length, 0)
+})
 
-const processMarkdown = (md, opts) => {
-  return remark()
-    .use(plugin, opts)
-    .process(md);
-};
+// Test('supports mix of valid and invalid links', async () => {
+//   await quibble.e
 
-describe('remark-lint-no-dead-urls', () => {
-  test('works with no URLs', () => {
-    const lint = processMarkdown(dedent`
-      # Title
+//   const lint = await processMarkdown(
+//     `
+// # Title
 
-      No URLs in here.
-    `);
+// Here is a [good link](https://www.github.com).
 
-    return lint.then((vFile) => {
-      expect(checkLinks).toHaveBeenCalledTimes(1);
-      expect(vFile.messages.length).toBe(0);
-    });
-  });
+// Here is a [bad link](https://github.com/unified/oops).
 
-  test('works', () => {
-    const lint = processMarkdown(
-      dedent`
-      # Title
+// Here is a [local link](http://localhost:3000).
+//   `,
+//     {
+//       gotOptions: {
+//         retry: {
+//           limit: 0
+//         }
+//       }
+//     }
+//   );
+// })
 
-      Here is a [good link](https://www.github.com).
+//     checkLinks.mockReturnValue(
+//       Promise.resolve({
+//         'https://www.github.com': { status: 'alive', statusCode: 200 },
+//         'https://github.com/unified/oops': {
+//           status: 'dead',
+//           statusCode: 404
+//         },
+//         'http://localhost:3000': { status: 'dead', statusCode: 404 }
+//       })
+//     );
 
-      Here is a [bad link](https://github.com/unified/oops).
+//     return lint.then((vFile) => {
+//       expect(checkLinks).toHaveBeenCalledTimes(1);
+//       expect(checkLinks.mock.calls[0][0]).toEqual([
+//         'https://www.github.com',
+//         'https://github.com/unified/oops',
+//         'http://localhost:3000'
+//       ]);
 
-      Here is a [local link](http://localhost:3000).
-    `,
-      {
-        gotOptions: {
-          retry: 0
-        }
-      }
-    );
+//       expect(vFile.messages.length).toBe(2);
+//       expect(vFile.messages[0].reason).toBe(
+//         'Link to https://github.com/unified/oops is dead'
+//       );
+//       expect(vFile.messages[1].reason).toBe(
+//         'Link to http://localhost:3000 is dead'
+//       );
+//     });
+//   }, 15000);
 
-    checkLinks.mockReturnValue(
-      Promise.resolve({
-        'https://www.github.com': { status: 'alive', statusCode: 200 },
-        'https://github.com/unified/oops': {
-          status: 'dead',
-          statusCode: 404
-        },
-        'http://localhost:3000': { status: 'dead', statusCode: 404 }
-      })
-    );
+//   test('works with definitions and images', () => {
+//     const lint = processMarkdown(
+//       dedent`
+//       # Title
 
-    return lint.then((vFile) => {
-      expect(checkLinks).toHaveBeenCalledTimes(1);
-      expect(checkLinks.mock.calls[0][0]).toEqual([
-        'https://www.github.com',
-        'https://github.com/unified/oops',
-        'http://localhost:3000'
-      ]);
+//       Here is a good pig: ![picture of pig](/pig-photos/384).
 
-      expect(vFile.messages.length).toBe(2);
-      expect(vFile.messages[0].reason).toBe(
-        'Link to https://github.com/unified/oops is dead'
-      );
-      expect(vFile.messages[1].reason).toBe(
-        'Link to http://localhost:3000 is dead'
-      );
-    });
-  }, 15000);
+//       Download the pig picture [here](/pig-photos/384).
 
-  test('works with definitions and images', () => {
-    const lint = processMarkdown(
-      dedent`
-      # Title
+//       Here is a [bad link]. Here is that [bad link] again.
 
-      Here is a good pig: ![picture of pig](/pig-photos/384).
+//       [bad link]: /oops/broken
+//     `,
+//       {
+//         gotOptions: {
+//           baseUrl: 'http://my.domain.com'
+//         }
+//       }
+//     );
 
-      Download the pig picture [here](/pig-photos/384).
+//     checkLinks.mockReturnValue(
+//       Promise.resolve({
+//         '/pig-photos/384': { status: 'alive', statusCode: 200 },
+//         '/oops/broken': { status: 'dead', statusCode: 404 }
+//       })
+//     );
 
-      Here is a [bad link]. Here is that [bad link] again.
+//     return lint.then((vFile) => {
+//       expect(checkLinks).toHaveBeenCalledTimes(1);
+//       expect(checkLinks.mock.calls[0][0]).toEqual([
+//         '/pig-photos/384',
+//         '/oops/broken'
+//       ]);
+//       expect(checkLinks.mock.calls[0][1]).toEqual({
+//         baseUrl: 'http://my.domain.com'
+//       });
+//       expect(vFile.messages.length).toBe(1);
+//       expect(vFile.messages[0].reason).toBe('Link to /oops/broken is dead');
+//     });
+//   });
 
-      [bad link]: /oops/broken
-    `,
-      {
-        gotOptions: {
-          baseUrl: 'http://my.domain.com'
-        }
-      }
-    );
+//   test('skips URLs with unsupported protocols', () => {
+//     const lint = processMarkdown(dedent`
+//       [Send me an email.](mailto:me@me.com)
+//       [Look at this file.](ftp://path/to/file.txt)
+//       [Special schema.](flopper://a/b/c)
+//     `);
 
-    checkLinks.mockReturnValue(
-      Promise.resolve({
-        '/pig-photos/384': { status: 'alive', statusCode: 200 },
-        '/oops/broken': { status: 'dead', statusCode: 404 }
-      })
-    );
+//     return lint.then((vFile) => {
+//       expect(checkLinks).toHaveBeenCalledTimes(1);
+//       expect(checkLinks.mock.calls[0][0]).toEqual([
+//         'mailto:me@me.com',
+//         'ftp://path/to/file.txt',
+//         'flopper://a/b/c'
+//       ]);
+//       expect(checkLinks.mock.calls[0][1]).toEqual(undefined);
+//       expect(vFile.messages.length).toBe(0);
+//     });
+//   });
 
-    return lint.then((vFile) => {
-      expect(checkLinks).toHaveBeenCalledTimes(1);
-      expect(checkLinks.mock.calls[0][0]).toEqual([
-        '/pig-photos/384',
-        '/oops/broken'
-      ]);
-      expect(checkLinks.mock.calls[0][1]).toEqual({
-        baseUrl: 'http://my.domain.com'
-      });
-      expect(vFile.messages.length).toBe(1);
-      expect(vFile.messages[0].reason).toBe('Link to /oops/broken is dead');
-    });
-  });
+//   test('warns if you are not online', () => {
+//     isOnline.mockReturnValueOnce(Promise.resolve(false));
 
-  test('skips URLs with unsupported protocols', () => {
-    const lint = processMarkdown(dedent`
-      [Send me an email.](mailto:me@me.com)
-      [Look at this file.](ftp://path/to/file.txt)
-      [Special schema.](flopper://a/b/c)
-    `);
+//     const lint = processMarkdown(dedent`
+//       Here is a [bad link](https://github.com/davidtheclark/oops).
+//     `);
 
-    return lint.then((vFile) => {
-      expect(checkLinks).toHaveBeenCalledTimes(1);
-      expect(checkLinks.mock.calls[0][0]).toEqual([
-        'mailto:me@me.com',
-        'ftp://path/to/file.txt',
-        'flopper://a/b/c'
-      ]);
-      expect(checkLinks.mock.calls[0][1]).toEqual(undefined);
-      expect(vFile.messages.length).toBe(0);
-    });
-  });
+//     return lint.then((vFile) => {
+//       expect(vFile.messages.length).toBe(1);
+//       expect(vFile.messages[0].reason).toMatch('You are not online');
+//     });
+//   });
 
-  test('warns if you are not online', () => {
-    isOnline.mockReturnValueOnce(Promise.resolve(false));
+//   test('skipOffline: true', () => {
+//     isOnline.mockReturnValueOnce(Promise.resolve(false));
 
-    const lint = processMarkdown(dedent`
-      Here is a [bad link](https://github.com/davidtheclark/oops).
-    `);
+//     const lint = processMarkdown(
+//       dedent`
+//       Here is a [bad link](https://github.com/davidtheclark/oops).
+//     `,
+//       {
+//         skipOffline: true
+//       }
+//     );
 
-    return lint.then((vFile) => {
-      expect(vFile.messages.length).toBe(1);
-      expect(vFile.messages[0].reason).toMatch('You are not online');
-    });
-  });
+//     return lint.then((vFile) => {
+//       expect(vFile.messages.length).toBe(0);
+//     });
+//   });
 
-  test('skipOffline: true', () => {
-    isOnline.mockReturnValueOnce(Promise.resolve(false));
+//   describe('skipLocalhost: true', () => {
+//     test('localhost', () => {
+//       const lint = processMarkdown(
+//         dedent`
+//           - [http://localhost](http://localhost)
+//           - [http://localhost/alex/test](http://localhost/alex/test)
+//           - [http://localhost:3000](http://localhost:3000)
+//           - [http://localhost:3000/alex/test](http://localhost:3000/alex/test)
+//           - [https://localhost](http://localhost)
+//           - [https://localhost/alex/test](http://localhost/alex/test)
+//           - [https://localhost:3000](http://localhost:3000)
+//           - [https://localhost:3000/alex/test](http://localhost:3000/alex/test)
+//         `,
+//         {
+//           skipLocalhost: true
+//         }
+//       );
 
-    const lint = processMarkdown(
-      dedent`
-      Here is a [bad link](https://github.com/davidtheclark/oops).
-    `,
-      {
-        skipOffline: true
-      }
-    );
+//       return lint.then((vFile) => {
+//         expect(vFile.messages.length).toBe(0);
+//       });
+//     });
 
-    return lint.then((vFile) => {
-      expect(vFile.messages.length).toBe(0);
-    });
-  });
+//     test('local IP 127.0.0.1', () => {
+//       const lint = processMarkdown(
+//         dedent`
+//           - [http://127.0.0.1](http://127.0.0.1)
+//           - [http://127.0.0.1:3000](http://127.0.0.1:3000)
+//           - [http://127.0.0.1/alex/test](http://127.0.0.1)
+//           - [http://127.0.0.1:3000/alex/test](http://127.0.0.1:3000)
+//           - [https://127.0.0.1](http://127.0.0.1)
+//           - [https://127.0.0.1:3000](http://127.0.0.1:3000)
+//           - [https://127.0.0.1/alex/test](http://127.0.0.1)
+//           - [https://127.0.0.1:3000/alex/test](http://127.0.0.1:3000)
+//         `,
+//         {
+//           skipLocalhost: true
+//         }
+//       );
 
-  describe('skipLocalhost: true', () => {
-    test('localhost', () => {
-      const lint = processMarkdown(
-        dedent`
-          - [http://localhost](http://localhost)
-          - [http://localhost/alex/test](http://localhost/alex/test)
-          - [http://localhost:3000](http://localhost:3000)
-          - [http://localhost:3000/alex/test](http://localhost:3000/alex/test)
-          - [https://localhost](http://localhost)
-          - [https://localhost/alex/test](http://localhost/alex/test)
-          - [https://localhost:3000](http://localhost:3000)
-          - [https://localhost:3000/alex/test](http://localhost:3000/alex/test)
-        `,
-        {
-          skipLocalhost: true
-        }
-      );
+//       return lint.then((vFile) => {
+//         expect(vFile.messages.length).toBe(0);
+//       });
+//     });
+//   });
 
-      return lint.then((vFile) => {
-        expect(vFile.messages.length).toBe(0);
-      });
-    });
+//   test.each([
+//     '[Ignore this](http://www.url-to-ignore.com)',
+//     '[Ignore this](http://www.url-to-ignore.com/somePath)',
+//     '[Ignore this](http://www.url-to-ignore.com/somePath?withQuery=wow)',
+//     '[its complicated](http://url-to-ignore.com/somePath/maybe)'
+//   ])('skipUrlPatterns for content: %s', (markdownContent) => {
+//     const lint = processMarkdown(markdownContent, {
+//       skipUrlPatterns: [/^http:\/\/(.*)url-to-ignore\.com/]
+//     });
 
-    test('local IP 127.0.0.1', () => {
-      const lint = processMarkdown(
-        dedent`
-          - [http://127.0.0.1](http://127.0.0.1)
-          - [http://127.0.0.1:3000](http://127.0.0.1:3000)
-          - [http://127.0.0.1/alex/test](http://127.0.0.1)
-          - [http://127.0.0.1:3000/alex/test](http://127.0.0.1:3000)
-          - [https://127.0.0.1](http://127.0.0.1)
-          - [https://127.0.0.1:3000](http://127.0.0.1:3000)
-          - [https://127.0.0.1/alex/test](http://127.0.0.1)
-          - [https://127.0.0.1:3000/alex/test](http://127.0.0.1:3000)
-        `,
-        {
-          skipLocalhost: true
-        }
-      );
-
-      return lint.then((vFile) => {
-        expect(vFile.messages.length).toBe(0);
-      });
-    });
-  });
-
-  test.each([
-    '[Ignore this](http://www.url-to-ignore.com)',
-    '[Ignore this](http://www.url-to-ignore.com/somePath)',
-    '[Ignore this](http://www.url-to-ignore.com/somePath?withQuery=wow)',
-    '[its complicated](http://url-to-ignore.com/somePath/maybe)'
-  ])('skipUrlPatterns for content: %s', (markdownContent) => {
-    const lint = processMarkdown(markdownContent, {
-      skipUrlPatterns: [/^http:\/\/(.*)url-to-ignore\.com/]
-    });
-
-    return lint.then((vFile) => {
-      expect(vFile.messages.length).toBe(0);
-    });
-  });
-});
+//     return lint.then((vFile) => {
+//       expect(vFile.messages.length).toBe(0);
+//     });
+//   });
+// });
